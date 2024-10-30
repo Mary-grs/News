8e18773ca93ad7edf5f73acf6fef4c2df7550fb1
mmreza <1tapciran@gmail.com>
2024-10-30 11:34:03 +0330

    Add DashboardLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import SideImageLayout from "@/components/layout/SideImageLayout";
 import LoginForm from "@/components/sections/LoginForm";
+import DashboardLayout from "@/components/layout/DashboardLayout";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,9 +31,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SideImageLayout>
+        <DashboardLayout>
           <LoginForm />
-        </SideImageLayout>
+        </DashboardLayout>
       </body>
     </html>
   );
